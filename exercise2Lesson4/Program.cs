33078e8f9271c6a06867e83353c1661fb657d836
Roma <rprilipko3@inbox.ru>
2022-07-10 01:48:09 +0300

    4 задание

---
+++ exercise2Lesson4/Program.cs
@@ -0,0 +1,14 @@
+﻿void multiplicity(int a)
+{
+     if(a % 2 == 0)
+     Console.WriteLine(a);
+}
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+while(i <= number)
+    {
+    multiplicity(i);
+    i++;
+    }
+
