675cedb3eb7c0663d61da1b04da68cd8188456fd
Roma <rprilipko3@inbox.ru>
2022-07-10 00:41:11 +0300

    Задание 3

---
+++ exercise2Lesson3/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+    if(numberA % 2 == 0)
+    {
+        Console.Write("Чётное");
+
+    }
+    else
+    {
+        Console.Write("Нечётное");
+    }
+    
+
+    
