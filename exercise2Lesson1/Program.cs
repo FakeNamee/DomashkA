b304dc77d3e81dba053b930b3419c2bb974d3a82
Roma <rprilipko3@inbox.ru>
2022-07-10 00:01:08 +0300

    Первое задание

--- exercise2Lesson1/Program.cs
+++ exercise2Lesson1/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿Console.WriteLine("Введите два любых числа : ");
+Console.Write("a = ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("b = ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+    if(numberA > numberB)
+    {
+        Console.WriteLine("a > b");
+    }
+    else
+    {
+        Console.WriteLine("a < b");
+    }
