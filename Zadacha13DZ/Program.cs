3e164bb7b89fa882cbc7a18f413b06279e7ba942
Roma <rprilipko3@inbox.ru>
2022-07-12 15:49:23 +0300

    13 задача, вывод 3 цифры

---
+++ Zadacha13DZ/Program.cs
@@ -0,0 +1,19 @@
+﻿    Console.WriteLine("Введите число:");
+    Console.Write("a = ");
+    int a = Convert.ToInt32(Console.ReadLine());
+    int result = Math.Abs(a);
+    int temporary = (result / 100);
+
+    while (result > 1000)
+    {
+        result /= 10;
+    }
+
+    if (temporary == 0)
+    {
+        Console.WriteLine("третья цифра отсутствует");
+    }
+    else
+    {
+        Console.WriteLine($"{a} -> {result % 10}");
+    }
