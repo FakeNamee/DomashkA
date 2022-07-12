bbd1cda29f1ac6e774ce886df766b436fac64ad4
Roma <rprilipko3@inbox.ru>
2022-07-12 16:07:19 +0300

    15 задача дз, дни недели

---
+++ Zadacha15DZ/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число от 1 до 7 : ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number == 7 || number == 6)
+{
+   Console.WriteLine("Выходной"); 
+}
+else
+{
+    Console.WriteLine("Будний");
+}
