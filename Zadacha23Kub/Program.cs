c4ff204b5a6cad173ad2f818dd49303f2eb5a58c
Roma <rprilipko3@inbox.ru>
2022-07-16 16:27:06 +0300

    23 задача, таблица кубов

---
+++ Zadacha23Kub/Program.cs
@@ -0,0 +1,8 @@
+﻿Random random = new Random();
+int number = random.Next (1, 11);
+Console.WriteLine ("Число: " + number);
+
+for (int i=1; i<=number; i++)
+{
+    Console.WriteLine (i + " в кубе= " + Math.Pow (i, 3));
+}
