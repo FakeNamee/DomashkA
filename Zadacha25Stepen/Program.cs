8df34486bbfd5a5870d6de1242cee196b6b379b8
Roma <rprilipko3@inbox.ru>
2022-07-18 20:11:18 +0300

    задача 25 возведение в степень

---
+++ Zadacha25Stepen/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число 1");
+double numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 2");
+double numberB = Convert.ToInt32(Console.ReadLine());
+double result = 0;
+for (double i = 0; i <= numberB; i++)
+{
+    result = Math.Pow(numberA, numberB);
+}
+Console.WriteLine("Ответ = " + result);
