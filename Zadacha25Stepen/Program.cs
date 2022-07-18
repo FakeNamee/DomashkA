532cdf94c6a2ecf5ae6a9a68604178baeef14b3b
Roma <rprilipko3@inbox.ru>
2022-07-18 20:23:07 +0300

    задача 25 возведение в степень(добавил проверку)

--- Zadacha25Stepen/Program.cs
+++ Zadacha25Stepen/Program.cs
@@ -1,10 +1,17 @@
-﻿Console.WriteLine("Введите число 1");
+﻿Console.WriteLine("Введите натуральное число 1");
 double numberA = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите число 2");
+Console.WriteLine("Введите натуральное число 2");
 double numberB = Convert.ToInt32(Console.ReadLine());
 double result = 0;
 for (double i = 0; i <= numberB; i++)
 {
     result = Math.Pow(numberA, numberB);
 }
-Console.WriteLine("Ответ = " + result);
+if (result <= 0)
+{
+    Console.WriteLine("Введено ненатуральное число");
+}
+else
+{
+    Console.WriteLine("Ответ = " + result);
+}
