8467a6eaae67ef8485c3c258a91420932b21c340
Roma <rprilipko3@inbox.ru>
2022-07-18 21:18:53 +0300

    задача 29 сортировка массива

---
+++ Zadacha29Mass/Program.cs
@@ -0,0 +1,26 @@
+﻿int size = 8;
+Random random = new Random();
+int[] array = new int[size];
+int max = array[0];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = random.Next(0, 8);
+}
+for (int i = 0; i < array.Length; i++)
+{
+
+    for (int j = 0; j < array.Length - 1; j++)
+    {
+        if (array[j] > array[j + 1])
+        {
+            max = array[j];
+            array[j] = array[j + 1];
+            array[j + 1] = max;
+
+        }
+    }
+}
+foreach (int num in array)
+{
+Console. Write(num + " "); 
+}
